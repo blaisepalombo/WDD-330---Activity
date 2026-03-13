06b69adf6dd487f95bf4034425de41e8ceba754b
Blaise Palombo <blaisepalombo@users.noreply.github.com>
2026-03-12 18:20:43 -0600

    Refactor header and footer structure; implement dynamic loading
    - Removed hardcoded header and footer from index.html and product pages.
    - Created separate header.html and footer.html partials for better maintainability.
    - Added loadHeaderFooter function to dynamically load header and footer in relevant pages.
    - Updated cart.js, main.js, product.js, and checkout.js to utilize the new header and footer loading mechanism.
    - Adjusted image paths in cart item templates for consistency.
    - Ensured that the cart and product pages now reference the new header and footer structure.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -28,3 +28,51 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener('click', callback);
 }
+
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data = {},
+  callback,
+  position = 'afterbegin',
+  clear = true,
+) {
+  const template = await templateFn(data);
+
+  if (clear) {
+    parentElement.innerHTML = '';
+  }
+
+  parentElement.insertAdjacentHTML(position, template);
+
+  if (callback) {
+    callback(data);
+  }
+}
+
+export function loadTemplate(path) {
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+    throw new Error(`Template not found: ${path}`);
+  };
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = loadTemplate('/partials/header.html');
+  const footerTemplate = loadTemplate('/partials/footer.html');
+
+  const headerElement = document.querySelector('#main-header');
+  const footerElement = document.querySelector('#main-footer');
+
+  if (headerElement) {
+    await renderWithTemplate(headerTemplate, headerElement);
+  }
+
+  if (footerElement) {
+    await renderWithTemplate(footerTemplate, footerElement);
+  }
+}
